8446adc5b681cbc9b51a99103f30b36cfefb731d
Boris <jobsboris27@yandex.ru>
2025-09-01 12:47:04 +0300

    feat: add f flag

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -13,6 +13,11 @@ func main() {
 		Name:  "gendiff",
 		Usage: "Compares two configuration files and shows a difference.",
 		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Usage:   "output format (default: \"stylish\")",
+			},
 			&cli.BoolFlag{
 				Name:    "help",
 				Aliases: []string{"h"},
